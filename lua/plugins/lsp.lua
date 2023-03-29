8ecf06c0b02a0b4dbbbbf71582f934d92db6f690
willcl-ark <will@256k1.dev>
2023-03-29 10:11:03 +0100

    use yapf for Bitcoin Core instead of ruff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -145,15 +145,24 @@ return {
       local null_ls = require("null-ls")
       local sources = {
         null_ls.builtins.code_actions.shellcheck,
+        null_ls.builtins.diagnostics.gitlint,
+        null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.diagnostics.ruff.with({
           extra_args = {
             "--ignore E501", -- ignore line-too-long
             -- "--line-length 88",  -- same as black
           },
         }),
-        null_ls.builtins.formatting.ruff,
-        null_ls.builtins.diagnostics.gitlint,
-        null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.formatting.yapf.with({
+          condition = function(utils) -- Use yapf formatter for Bitcoin Core
+            return utils.root_matches("bitcoin") ~= nil
+          end,
+        }),
+        null_ls.builtins.formatting.ruff.with({
+          condition = function(utils) -- Don't use ruff formatter with Bitcoin Core
+            return utils.root_matches("bitcoin") == nil
+          end,
+        }),
         -- null_ls.builtins.formatting.black.with({
         --   extra_args = { "--line-length=120" },
         -- }),
