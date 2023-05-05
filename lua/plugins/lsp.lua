e71c1f757eaf342e7ae6d1a36e586b6bf5c2231c
willcl-ark <will@256k1.dev>
2023-05-05 09:54:03 +0100

    add gitsigns keybinds and code action

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -161,6 +161,7 @@ return {
       local null_ls = require("null-ls")
       local sources = {
         null_ls.builtins.code_actions.shellcheck,
+        null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.diagnostics.gitlint,
         null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.diagnostics.ruff.with({
