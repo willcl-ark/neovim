84a61ce29175ef637d5a276c6978b0033c7f211c
willcl-ark <will@256k1.dev>
2023-10-02 20:38:11 +0100

    lastplace: use opts struct

--- lua/plugins/lastplace.lua
+++ lua/plugins/lastplace.lua
@@ -1,12 +1,10 @@
 return {
   -- Open files in last edited place
   "ethanholz/nvim-lastplace",
-  branch="main",
-  config = function()
-    require("nvim-lastplace").setup({
-      lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
-      lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
-      lastplace_open_folds = true,
-    })
-  end,
+  branch = "main",
+  opts = {
+    lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+    lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+    lastplace_open_folds = true,
+  },
 }
