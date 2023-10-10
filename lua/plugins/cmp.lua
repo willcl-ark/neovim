7d20fc03a63706c5ea8f0c11c254b4522f6f7e55
willcl-ark <will@256k1.dev>
2023-10-10 15:00:02 +0100

    feat(cmp): ctrl+CR will abort cmp and enter a new line

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -69,10 +69,11 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<S-CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
         }),
         sorting = {
           comparators = {
