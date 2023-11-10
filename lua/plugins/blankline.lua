28fda8a8b4a60066ec388981466a2ff2ed6f10d2
willcl-ark <will@256k1.dev>
2023-11-10 21:38:33 +0000

    indentblankline: update to v3

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,27 +1,31 @@
 return {
-  "lukas-reineke/indent-blankline.nvim", -- Add indentation guides even on blank lines
-  -- See `:help indent_blankline.txt`
-  event = { "BufReadPost", "BufNewFile" },
-  tag = "v2.20.8",
+  "lukas-reineke/indent-blankline.nvim",
+  event = "BufReadPost",
+  main = "ibl",
   config = function()
-    vim.opt.list = true
-    vim.cmd([[highlight IndentBlanklineIndent1 guifg=#f38ba8 gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent2 guifg=#f9e2af gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent3 guifg=#a6e3a1 gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent4 guifg=#89b4fa gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent5 guifg=#f5c2e7 gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent6 guifg=#89dceb gui=nocombine]])
-    require("indent_blankline").setup({
-      show_current_context = true,
-      show_current_context_start = true,
-      char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-      },
-    })
+    local highlight = {
+      "Red",
+      "Yellow",
+      "Blue",
+      "Orange",
+      "Green",
+      "Violet",
+      "Cyan",
+    }
+
+    local hooks = require("ibl.hooks")
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "Red", { fg = "#EA6962" })
+      vim.api.nvim_set_hl(0, "Yellow", { fg = "#D8A657" })
+      vim.api.nvim_set_hl(0, "Blue", { fg = "#7DAEA3" })
+      vim.api.nvim_set_hl(0, "Orange", { fg = "#E78A4E" })
+      vim.api.nvim_set_hl(0, "Green", { fg = "#A9B665" })
+      vim.api.nvim_set_hl(0, "Violet", { fg = "#D3869B" })
+      vim.api.nvim_set_hl(0, "Cyan", { fg = "#89B482" })
+    end)
+
+    require("ibl").setup({ indent = { highlight = highlight, char = { "│" } }})
   end,
 }
