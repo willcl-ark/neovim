adee71878b0137d0c03b8edd1888deca71e52696
willcl-ark <will@256k1.dev>
2024-02-12 15:28:20 +0000

    just: add tree-sitter-just anv vim-just!

---
+++ lua/plugins/just.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "NoahTheDuke/vim-just",
+        ft = { "just" },
+    },
+    {
+        "IndianBoy42/tree-sitter-just",
+        config = true,
+        ft = { "just" },
+    }
+}
