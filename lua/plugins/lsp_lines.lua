f7912b62c805721685ca4b79e2f7d6f70a5ad843
willcl-ark <will@256k1.dev>
2024-02-12 15:27:16 +0000

    lsp_lines: temp disable

--- lua/plugins/lsp_lines.lua
+++ lua/plugins/lsp_lines.lua
@@ -1,5 +1,6 @@
 return {
   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  enable = false,
   config = function()
     require("lsp_lines").setup({
       vim.diagnostic.config({ virtual_text = true }),
