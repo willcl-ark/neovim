52f75b1932552a12f6a47fa3fc0ef9fb2c7082d0
willcl-ark <will@256k1.dev>
2024-03-08 20:55:56 +0000

    fixup colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,17 +1,12 @@
 return {
-  "NvChad/nvim-colorizer.lua",
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    require("colorizer").setup({ "*" }, {
-      RGB = true, -- #RGB hex codes
-      RRGGBB = true, -- #RRGGBB hex codes
-      names = false, -- "Name" codes like Blue
-      RRGGBBAA = true, -- #RRGGBBAA hex codes
-      rgb_fn = true, -- CSS rgb() and rgba() functions
-      hsl_fn = true, -- CSS hsl() and hsla() functions
-      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      mode = "background", -- Set the display mode.
-    })
-  end,
+    "NvChad/nvim-colorizer.lua",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+        user_default_options = {
+            RGB = true,
+            names = false,
+            RRGGBB = true,
+            AARRGGBB = false,
+        },
+    }
 }
