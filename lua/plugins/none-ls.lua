57dbc7db9e5c5a8bb8988da67f04ff8193d6985e
willcl-ark <will@256k1.dev>
2024-04-11 20:03:57 +0200

    Fix none-ls deprecating modules

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,31 +1,24 @@
 return {
-  {
-    -- Extra code_actions and diagnostics
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "gbprod/none-ls-shellcheck.nvim",
-      "nvimtools/none-ls-extras.nvim",
-    },
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local null_ls = require("null-ls")
-      local sources = {
-        require("none-ls-shellcheck.diagnostics"),
-        require("none-ls-shellcheck.code_actions"),
-        null_ls.builtins.code_actions.gitsigns,
-        null_ls.builtins.diagnostics.gitlint,
-      }
+	{
+		-- Extra code_actions and diagnostics
+		"nvimtools/none-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("null-ls").register(require("none-ls-shellcheck.diagnostics"))
+			require("null-ls").register(require("none-ls-shellcheck.code_actions"))
+			local null_ls = require("null-ls")
+			local sources = {
+				null_ls.builtins.code_actions.gitsigns,
+				null_ls.builtins.diagnostics.gitlint,
+			}
 
-      null_ls.setup({
-        debug = false,
-        sources = sources,
-      })
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "gbprod/none-ls-shellcheck.nvim",
-    },
-  },
+			null_ls.setup({
+				debug = false,
+				sources = sources,
+			})
+		end,
+		dependencies = {
+			"gbprod/none-ls-shellcheck.nvim",
+		},
+	},
 }
