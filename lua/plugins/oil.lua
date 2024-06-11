9b3b07d339e5292216846630e59ab862063ce766
willcl-ark <will@256k1.dev>
2024-06-11 22:15:56 +0100

    Add oil.nvim

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    opts = {
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      },
+    },
+    -- Optional dep
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+}
