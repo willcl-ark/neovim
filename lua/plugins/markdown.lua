428ef667af3264f129168b1550fd96acaf136835
willcl-ark <will@256k1.dev>
2024-08-13 13:35:19 +0100

    fixup! add markdown.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,14 +1,8 @@
 return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,      -- Recommended
-    -- ft = "markdown" -- If you decide to lazy-load anyway
-
-    dependencies = {
-        -- You will not need this if you installed the
-        -- parsers manually
-        -- Or if the parsers are in your $RUNTIMEPATH
-        -- "nvim-treesitter/nvim-treesitter",
-        --
-        -- "nvim-tree/nvim-web-devicons"
-    }
+  "OXY2DEV/markview.nvim",
+  lazy = true,
+  ft = "markdown",
+  keys = {
+    { "<leader>m", "<cmd>Markview toggle<cr>", desc = "Toggle Markview" },
+  },
 }
