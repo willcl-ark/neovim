6ba22d636ffd8c38fcccfcc0a3689b9ddca634de
willcl-ark <will@256k1.dev>
2024-08-14 19:05:28 +0100

    fixup! fixup! add markdown.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -5,4 +5,11 @@ return {
   keys = {
     { "<leader>m", "<cmd>Markview toggle<cr>", desc = "Toggle Markview" },
   },
+  config = function()
+    require("markview").setup()
+    -- Disable Markview by default
+    require("markview").state.enable = false
+    -- Alternatively, you can use the command:
+    -- vim.cmd("Markview disableAll")
+  end,
 }
