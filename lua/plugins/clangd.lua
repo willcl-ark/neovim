823cc338fe4d8231102a7f1838c9b19c4c3725c9
willcl-ark <will@256k1.dev>
2024-12-10 22:51:20 +0000

    fix clangd sorting

--- lua/plugins/clangd.lua
+++ lua/plugins/clangd.lua
@@ -77,8 +77,9 @@ return {
   {
     "nvim-cmp",
     opts = function(_, opts)
+      opts.sorting = opts.sorting or {}
+      opts.sorting.comparators = opts.sorting.comparators or {}
       table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
     end,
   },
-
 }
