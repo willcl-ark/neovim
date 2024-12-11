56c5fc022fb0f497613e7feffad6652e80c6602f
willcl-ark <will@256k1.dev>
2024-12-11 11:22:22 +0000

    disable avante hints

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -3,20 +3,22 @@ return {
   event = "VeryLazy",
   build = "make BUILD_FROM_SOURCE=true luajit",
   opts = {
-    -- add any opts here
+    ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+    provider = "claude",
+    auto_suggestions_provider = "claude",
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-3-5-sonnet-latest",
+      temperature = 0.2, -- This could be set to "0" for programming, potentially. But 0.2 gets us a little "creativity"
+      max_tokens = 4096,
+    },
+    hints = { enabled = false },
   },
   dependencies = {
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    -- The below is optional, make sure to setup it properly if you have lazy=true
-    -- {
-    --   'MeanderingProgrammer/render-markdown.nvim',
-    --   opts = {
-    --     file_types = { "markdown", "Avante" },
-    --   },
-    --   ft = { "markdown", "Avante" },
-    -- },
+    "hrsh7th/nvim-cmp",
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "stevearc/dressing.nvim",
   },
 }
