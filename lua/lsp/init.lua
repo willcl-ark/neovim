1ea11a40737df7639839c10873b89b1016906967
willcl-ark <will@256k1.dev>
2024-12-12 09:40:22 +0000

    tidy up telescope/lsp code

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -15,14 +15,16 @@ function M.client_capabilities()
 end
 
 -- LSP keymaps and autocommands
-function M.on_attach(client, bufnr)
+function M.on_attach(_, bufnr)
+  -- stylua: ignore start
   -- Diagnostic keymaps
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+  vim.keymap.set("n", "[d",        vim.diagnostic.goto_prev)
+  vim.keymap.set("n", "]d",        vim.diagnostic.goto_next)
   vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
   vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
-  vim.keymap.set("n", "<leader>d", vim.diagnostic.disable)
-  vim.keymap.set("n", "<leader>de", vim.diagnostic.enable)
+  vim.keymap.set("n", "<leader>d", function()
+    vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+  end)
 
   -- LSP keymaps
   local nmap = function(keys, func, desc)
@@ -30,27 +32,29 @@ function M.on_attach(client, bufnr)
   end
 
   -- Core LSP functionality
-  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+  nmap("<leader>rn", vim.lsp.buf.rename,                      "[R]e[n]ame")
+  nmap("<leader>ca", vim.lsp.buf.code_action,                 "[C]ode [A]ction")
+  nmap("K",          vim.lsp.buf.hover,                       "Hover Do[K]umentation")
+  nmap("<C-k>",      vim.lsp.buf.signature_help,              "Signature Do[K]umentation")
+  nmap("gD",         vim.lsp.buf.declaration,                 "[G]oto [D]eclaration")
 
   -- Telescope-based LSP actions
   local telescope = require("telescope.builtin")
-  nmap("gd", telescope.lsp_definitions, "[G]oto [D]efinitions")
-  nmap("gr", telescope.lsp_references, "[G]oto [R]eferences")
-  nmap("gI", telescope.lsp_implementations, "[G]oto [I]mplementations")
-  nmap("<leader>D", telescope.lsp_type_definitions, "Type [D]efinitions")
-  nmap("<leader>gs", telescope.lsp_document_symbols, "[G]oto [S]ymbols")
+  nmap("gr",         telescope.lsp_references,                "[G]oto [R]eferences")
+  nmap("<leader>gs", telescope.lsp_document_symbols,          "[G]oto [S]ymbols")
   nmap("<leader>ws", telescope.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+  nmap("<leader>sd", telescope.diagnostics,                   "[S]earch [D]iagnostics")
+  nmap("gI",         telescope.lsp_implementations,           "[G]oto [I]mplementations")
+  nmap("gd",         telescope.lsp_definitions,               "[G]oto [D]efinitions")
+  nmap("gt",         telescope.lsp_type_definitions,          "[G]oto [T]ype Definition")
 
   -- Workspace folder management
-  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder,        "[W]orkspace [A]dd Folder")
+  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder,     "[W]orkspace [R]emove Folder")
   nmap("<leader>wl", function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, "[W]orkspace [L]ist Folders")
+  end,                                                        "[W]orkspace [L]ist Folders")
+  -- stylua: ignore end
 end
 
 -- Dynamic capability registration
