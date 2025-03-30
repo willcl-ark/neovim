8e3d32f0ae50c5e191d867de97a8795174154018
willcl-ark <will@256k1.dev>
2025-03-30 18:54:13 +0100

    stick to 3.5; it's less mad

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -8,7 +8,7 @@ return {
     auto_suggestions_provider = "claude",
     claude = {
       endpoint = "https://api.anthropic.com",
-      model = "claude-3-7-sonnet-latest",
+      model = "claude-3-5-sonnet-latest",
       temperature = 0.2, -- This could be set to "0" for programming, potentially. But 0.2 gets us a little "creativity"
       max_tokens = 4096,
     },
