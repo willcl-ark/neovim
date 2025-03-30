31598a2d234d49dcbbf692116a85edec03e86566
willcl-ark <will@256k1.dev>
2025-03-30 22:27:05 +0100

    simplify conform config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,10 +3,6 @@ return {
     "stevearc/conform.nvim",
     cmd = { "ConformInfo" },
     event = "BufEnter",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "zapling/mason-conform.nvim",
-    },
     keys = {
       {
         "<leader>f",
@@ -51,50 +47,6 @@ return {
     },
     init = function()
       vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
-    config = function(_, opts)
-      -- First set up conform with the options
-      require("conform").setup(opts)
-
-      -- Map of formatter names to their executable names
-      local formatter_binaries = {
-        clang_format = "clang-format",
-        fish_indent = "fish_indent",
-        gofmt = "gofmt",
-        mdformat = "mdformat",
-        ruff_format = "ruff",
-        rustfmt = "rustfmt",
-        shfmt = "shfmt",
-        shellcheck = "shellcheck",
-        stylua = "stylua",
-        yamlfmt = "yamlfmt",
-      }
-
-      -- Func to check if a command exists in PATH
-      local function command_exists(cmd)
-        local handle = io.popen("command -v " .. cmd .. ' >/dev/null 2>&1 && echo "true" || echo "false"')
-        if handle then
-          local result = handle:read("*l")
-          handle:close()
-          return result == "true"
-        end
-        return false
-      end
-
-      -- Collect formatters that need to be installed
-      local ignore_install = {}
-      for formatter, binary in pairs(formatter_binaries) do
-        if command_exists(binary) then
-          table.insert(ignore_install, formatter)
-        else
-          vim.notify("Will install " .. formatter .. " via Mason (not found in PATH)", vim.log.levels.INFO)
-        end
-      end
-
-      -- Set up mason-conform to ignore formatters we already have
-      require("mason-conform").setup({
-        ignore_install = ignore_install,
-      })
     end,
   },
 }
