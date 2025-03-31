b146b652e8f5585862a8666962e92decaf0fd7cd
willcl-ark <will@256k1.dev>
2025-03-31 08:41:52 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Neovim with LSPs and formatters";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            neovim
+
+            # General, Telescope
+            git
+            ripgrep
+            fd
+
+            # LSPs
+            # C++
+            clang-tools  # Provides clangd and clang-format
+            cmake
+
+            # Rust
+            rust-analyzer
+            rustfmt
+
+            # Python
+            pyright
+            ruff
+
+            # Go
+            gopls
+            go  # Provides gofmt
+
+            # Zig
+            zls
+
+            # Shell(s)
+            fish-lsp
+            shellcheck
+            shfmt
+
+            # Misc
+            codespell
+            gitlint
+            mdformat
+            stylua
+          ];
+
+          shellHook = ''
+            echo "Neovim development environment loaded"
+          '';
+        };
+
+      }
+    );
+}
